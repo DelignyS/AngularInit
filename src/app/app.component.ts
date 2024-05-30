64367f7c23af6e41c223e1a8475149bd3465270e
Sylvain.D <60609924+JerkStanton@users.noreply.github.com>
2024-05-30 14:42:39 +0200

    JSR init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: ` <h1>Bonjour</h1> `,
+  styles: [
+    `
+      h1 {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'AngularInit';
